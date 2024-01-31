d5a2fc84a2678abd40031a260ef6f123462bc519
MarkBelyi <123000957+MarkBelyi@users.noreply.github.com>
2024-01-31 15:14:03 +0600

    CreatePasswordScreen adaptive version_1

---
+++ app/src/main/java/com/example/walletapp/elements/checkbox/PasswordFieldWithLabel.kt
@@ -0,0 +1,104 @@
+package com.example.walletapp.elements.checkbox
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import com.example.walletapp.R
+import com.example.walletapp.ui.theme.roundedShape
+
+@Composable
+fun PasswordFieldWithLabel(
+    labelText: String,
+    onValueChange: (String) -> Unit,
+    passwordValue: String,
+    labelColor: Color,
+
+    onImeAction: () -> Unit
+) {
+    var isPasswordVisible by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 5.dp, bottom = 5.dp)
+    ) {
+        Text(
+            text = labelText,
+            style = TextStyle(color = labelColor)
+        )
+
+        Spacer(modifier = Modifier.height(5.dp))
+
+        OutlinedTextField(
+            value = passwordValue,
+            onValueChange = {
+                if (!it.contains("\n")) {
+                    onValueChange(it)
+                }
+            },
+            label = { Text(stringResource(id = R.string.password_name)) },
+            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    onImeAction() // Вызываем переданную функцию, когда пользователь нажимает "Done"
+                }
+            ),
+            textStyle = TextStyle(color = colorScheme.onBackground),
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(min = 56.dp, max = 72.dp),
+            shape = roundedShape,
+            singleLine = true,
+            trailingIcon = {
+                IconButton(onClick = {
+                    isPasswordVisible = !isPasswordVisible
+                }) {
+                    Icon(
+                        imageVector = if (isPasswordVisible) Icons.Rounded.Star else Icons.Outlined.Star,
+                        contentDescription = if (isPasswordVisible) "Hide password" else "Show password",
+                        tint = colorScheme.onBackground
+                    )
+                }
+            },
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = colorScheme.background,
+                focusedLabelColor = colorScheme.primary,
+                unfocusedContainerColor = colorScheme.background,
+                unfocusedLabelColor = colorScheme.onBackground,
+                cursorColor = colorScheme.primary
+            ),
+        )
+    }
+}
---
+++ app/src/main/java/com/example/walletapp/registrationScreens/CreatePasswordScreenConstraint.kt
@@ -0,0 +1,160 @@
+package com.example.walletapp.registrationScreens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.walletapp.R
+import com.example.walletapp.elements.checkbox.PasswordFieldWithLabel
+import com.example.walletapp.ui.theme.paddingColumn
+import com.example.walletapp.ui.theme.roundedShape
+
+@Composable
+fun CreatePasswordScreen(){
+    var showPasswordAlert by remember { mutableStateOf(false) }
+    var passwordAlertMessage by remember { mutableStateOf("") }
+    var passwordValue by remember { mutableStateOf("") }
+    var repeatPasswordValue by remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
+    val isPasswordValid = checkPasswordsMatch(passwordValue, repeatPasswordValue)
+    val passwordErrorMessage = stringResource(id = R.string.alert_password_message)
+
+
+    if (showPasswordAlert) {
+        AlertDialog(
+            onDismissRequest = { showPasswordAlert = false },
+            title = { Text(stringResource(id = R.string.error_password)) },
+            text = { Text(passwordAlertMessage) },
+            confirmButton = {
+                TextButton(
+                    onClick = { showPasswordAlert = false }
+                ) {
+                    Text("OK")
+                }
+            },
+            shape = RoundedCornerShape(25.dp)
+        )
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorScheme.background)
+            .padding(paddingColumn)
+    ){
+
+        Spacer(modifier = Modifier.weight(0.1f))
+
+        PasswordFieldWithLabel(
+            labelText = stringResource(id = R.string.new_password),
+            onValueChange = { newValue ->
+                passwordValue = newValue
+            },
+            passwordValue = passwordValue,
+            labelColor = colorScheme.onBackground,
+            onImeAction = { focusManager.moveFocus(FocusDirection.Down) }
+        )
+
+        Spacer(modifier = Modifier.weight(0.005f))
+
+        PasswordFieldWithLabel(
+            labelText = stringResource(id = R.string.repeat_password),
+            onValueChange = { newValue ->
+                repeatPasswordValue = newValue
+            },
+            passwordValue = repeatPasswordValue,
+            labelColor = colorScheme.onBackground,
+            onImeAction = {
+                // Вызываем функцию проверки пароля, если пароль валиден
+                if (isPasswordValid(passwordValue)) {
+                    // ... код для обработки валидного пароля ...
+                } else {
+                    passwordAlertMessage = passwordErrorMessage
+                    showPasswordAlert = true
+                }
+            }
+        )
+
+        Spacer(modifier = Modifier.weight(0.05f))
+
+        CustomButton(
+            text = stringResource(id = R.string.button_continue),
+            enabled = isPasswordValid,
+            onClick = {
+                if (isPasswordValid(passwordValue)) {
+
+                } else {
+                    passwordAlertMessage = passwordErrorMessage
+                    showPasswordAlert = true
+                }
+            }
+        )
+
+        Spacer(modifier = Modifier.weight(0.8f))
+
+
+    }
+}
+
+
+fun checkPasswordsMatch(password1: String, password2: String): Boolean {
+    return password1 == password2 && password1.isNotEmpty()
+}
+
+fun isPasswordValid(password: String): Boolean {
+    val hasUpperCase = password.any { it.isUpperCase() }
+    val hasDigit = password.any { it.isDigit() }
+    val hasSpecialChar = password.any { !it.isLetterOrDigit() }
+    val isLongEnough = password.length >= 8
+
+    return hasUpperCase && hasDigit && hasSpecialChar && isLongEnough
+}
+
+@Composable
+fun CustomButton(
+    text: String,
+    enabled: Boolean,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    ElevatedButton(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp, max = 64.dp)
+            .padding(top = 5.dp, bottom = 5.dp),
+        enabled = enabled,
+        shape = roundedShape,
+        colors = ButtonDefaults.elevatedButtonColors(
+            containerColor = colorScheme.primary,
+            contentColor = colorScheme.onPrimary,
+            disabledContainerColor = colorScheme.primaryContainer,
+            disabledContentColor = colorScheme.onPrimaryContainer
+        )
+    ) {
+        Text(text = text)
+    }
+}
+
