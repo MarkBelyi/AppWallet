1b33e40083e7d4ee90b1b1d515fee39755743601
MarkBelyi <123000957+MarkBelyi@users.noreply.github.com>
2024-08-18 20:40:13 +0500

    Start of TronScanApi

---
+++ app/src/main/java/com/example/walletapp/TronScanApi/TronScanApi.kt
@@ -0,0 +1,30 @@
+package com.example.walletapp.TronScanApi
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+fun TronScanApi(){
+    val apiKey = "bbd6e3b5-09ed-4d2a-b429-df66d18d6006"
+    val endpoint = "https://apilist.tronscanapi.com/api/block"
+
+    try {
+        val url = URL(endpoint)
+        with(url.openConnection() as HttpURLConnection) {
+            requestMethod = "GET"
+            setRequestProperty("TRON-PRO-API-KEY", apiKey)
+
+            BufferedReader(InputStreamReader(inputStream)).use { reader ->
+                val responseBody = StringBuilder()
+                var line: String?
+                while (reader.readLine().also { line = it } != null) {
+                    responseBody.append(line)
+                }
+                println(responseBody.toString())
+            }
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
